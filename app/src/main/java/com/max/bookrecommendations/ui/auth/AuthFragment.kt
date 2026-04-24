7fe118b711d9b49f5f666e768567015d393a96d5
MaxHaGever <maxspectorr@gmail.com>
2026-04-24 14:42:42 +0300

    Initial project structure with fragments

---
+++ app/src/main/java/com/max/bookrecommendations/ui/auth/AuthFragment.kt
@@ -0,0 +1,18 @@
+package com.max.bookrecommendations.ui.auth
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.max.bookrecommendations.R
+
+class AuthFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.fragment_auth, container, false)
+    }
+}
